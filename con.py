4207f458aae339c12e9c67dee046fb0c4798b1bc
Bastien Gallay <bastien@gallay.org>
2021-10-01 10:42:53 +0200

    Premier message concomitant, avant de conpulser

--- con.py
+++ con.py
@@ -2,27 +2,44 @@
 # coding: utf-8
 
 
-import subprocess
-
 __version__ = '0.1.0'
 
+import subprocess
+import sys
+
 TRADUCTIONS = {
-    u'pull': u'tracte',
-    u"push": u"pulse",
-    u"clone": u"forme",
-    u"merge": u"bine",
-    u"rebase": u"fonde",
-    u"diff": u"pare",
-    u"log": u"signe"
+    u'tracte': u'pull',
+    u"pulse": u"push",
+    u"forme": u"clone",
+    u"bine": u"merge",
+    u"fonde": u"rebase",
+    u"pare": u"diff",
+    u"signe": u"log",
+    u"state": u"status",
+    u"comitant": u"commit"
 }
 
 
 def traduit_commande(commande):
-    return commande if commande not in TRADUCTIONS.keys() else TRADUCTIONS[commande]
+    if commande in TRADUCTIONS.keys():
+        return TRADUCTIONS[commande]
+    else:
+        return commande
+
+
+def remplace_commande_principale(commande):
+    commande[0] = 'git'
+    return commande
+
+
+def traduit_ligne_commande(ligne_commande):
+    return [traduit_commande(arg) for arg in ligne_commande]
 
 
 def main():
-    subprocess.call(['git'])
+    ligne_commande = remplace_commande_principale(sys.argv)
+    command_line_list = traduit_ligne_commande(ligne_commande)
+    subprocess.call(command_line_list)
 
 
 if __name__ == '__main__':  # pragma: no cover
--- tests/test_translation.py
+++ tests/test_translation.py
@@ -2,7 +2,7 @@
 import pytest
 
 from con import (
-    traduit_commande,
+    traduit_commande, remplace_commande_principale, traduit_ligne_commande,
 )
 
 
@@ -12,11 +12,24 @@ from con import (
     ("bine", "merge"),
     ("pare", "diff"),
     ("fonde", "rebase"),
-    ("signe", "log")
+    ("signe", "log"),
+    ("state", "status"),
+    ("comitant", "commit")
 ])
 def test_translate_push_to_pulse(nom, name):
-    assert nom == traduit_commande(name)
+    assert name == traduit_commande(nom)
 
 
 def test_translate_unknown_command_returns_itself():
     assert "notranslation" == traduit_commande("notranslation")
+
+
+def test_remplace_commande_principale_avec_git():
+    input_command_argv = ["toto", "titi", "tutu", "tata"]
+    expected_command_argv = ["git", "titi", "tutu", "tata"]
+    assert expected_command_argv == remplace_commande_principale(input_command_argv)
+
+def test_traduit_ligne_de_commande_complete():
+    input_command_argv = ["git", "pulse", "tutu", "tata"]
+    expected_command_argv = ["git", "push", "tutu", "tata"]
+    assert expected_command_argv == traduit_ligne_commande(input_command_argv)
